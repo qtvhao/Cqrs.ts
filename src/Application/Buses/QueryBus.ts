5dc6dc4b45a2562ad98b81af3011360bfa02ee7e
Hao Nghiem <qtvhao@gmail.com>
2025-05-26 14:14:13 +0700

    Add

--- src/Application/Buses/QueryBus.ts
+++ src/Application/Buses/QueryBus.ts
@@ -1,14 +1,13 @@
-import { IQuery, IQueryBus, IQueryHandler, IQueryHandlerResolver } from "contracts.ts";
+import { IQuery, IQueryBus, IQueryHandlerResolver } from "contracts.ts";
+import { IQueryResult } from "contracts.ts/dist/Application/DTOs/IQueryResult";
 
 export type QueryConstructor<T extends IQuery = IQuery> = new (...args: any[]) => T;
 
 export class QueryBus implements IQueryBus {
-    constructor(private resolver: IQueryHandlerResolver) { }
+  constructor(private readonly resolver: IQueryHandlerResolver) {}
 
-    async execute<T extends IQuery, R>(query: T): Promise<R> {
-        return {
-            results: {},
-            query,
-        } as R;
-    }
+  async execute<T extends IQuery, R extends IQueryResult>(query: T): Promise<R | void> {
+    const handler = this.resolver.resolve<T, R>(query.constructor as any);
+    return await handler.execute(query);
+  }
 }
