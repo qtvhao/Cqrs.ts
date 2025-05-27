c8ef366c8f584995cce1a3f70ed356acd43ffb37
Hao Nghiem <qtvhao@gmail.com>
2025-05-27 12:59:07 +0700

    Add

--- src/Application/Buses/QueryBus.ts
+++ src/Application/Buses/QueryBus.ts
@@ -1,13 +1,23 @@
-import { IQuery, IQueryBus, IQueryHandlerResolver } from "contracts.ts";
+import { IQuery, IQueryBus, IQueryHandlerResolver, TYPES } from "contracts.ts";
 import { IQueryResult } from "contracts.ts/dist/Application/DTOs/IQueryResult";
+import { inject, injectable } from "inversify";
 
-export type QueryConstructor<T extends IQuery = IQuery> = new (...args: any[]) => T;
+export type QueryConstructor<T extends IQuery = IQuery> = new (
+  ...args: any[]
+) => T;
 
+@injectable()
 export class QueryBus implements IQueryBus {
-  constructor(private readonly resolver: IQueryHandlerResolver) {}
+  constructor(
+    @inject(TYPES.QueryBus) private readonly resolver: IQueryHandlerResolver,
+  ) {}
 
-  async execute<T extends IQuery, R extends IQueryResult>(query: T): Promise<R | void> {
-    const handler = this.resolver.resolve<T, R>(query.constructor as QueryConstructor<T>);
+  async execute<T extends IQuery, R extends IQueryResult>(
+    query: T,
+  ): Promise<R | void> {
+    const handler = this.resolver.resolve<T, R>(
+      query.constructor as QueryConstructor<T>,
+    );
     return await handler.execute(query);
   }
 }
